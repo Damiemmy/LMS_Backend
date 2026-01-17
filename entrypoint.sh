f01493ce9aa8a279a3790370b7d0a1394442b245
damiemmy <damisaemmanuel778@gmail.com>
2026-01-17 03:07:24 -0800

    modified entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,8 +5,8 @@ echo "Collecting static files"
 python manage.py collectstatic --clear --noinput
 
 echo "Running migrations"
-python manage.py migrate --noinput
+python manage.py migrate 
 
 echo "Starting Gunicorn"
-PORT=${PORT:-8000}
-exec gunicorn backend.wsgi:application --bind 0.0.0.0:$PORT --workers 3 --timeout 120
+
+exec gunicorn backend.wsgi:application --bind 0.0.0.0:8000
