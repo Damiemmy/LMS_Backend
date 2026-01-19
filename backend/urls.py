fd320d8f45d87e91b2288b2e1daae4d3e3de2550
damiemmy <damisaemmanuel778@gmail.com>
2026-01-19 09:31:00 -0800

    urls.config

--- backend/urls.py
+++ backend/urls.py
@@ -25,11 +25,11 @@ from drf_yasg import openapi
 
 schema_view = get_schema_view(
    openapi.Info(
-      title="Desphixs LMS Backend APIs",
+      title="Damie LMS Backend APIs",
       default_version='v1',
-      description="This is the API documentation for Desphixs LMS project APIs",
+      description="This is the API documentation for Damie LMS project APIs",
       terms_of_service="https://www.google.com/policies/terms/",
-      contact=openapi.Contact(email="destiny@gmail.com"),
+      contact=openapi.Contact(email="damisaemmanuel778@gmail.com"),
       license=openapi.License(name="BSD License"),
    ),
    public=True,
